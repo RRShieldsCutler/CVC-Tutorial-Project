f05b6bd4b69383e8a2ae788a9e88d7b7dfc66613
Robin Shields-Cutler <cutler.robin@gmail.com>
2017-06-14 15:16:14 -0500

    Initial data frame setup, split by taxonomic rank

--- bin/data_practice.R
+++ bin/data_practice.R
@@ -4,3 +4,19 @@ library(vegan)
 library(ggplot2)
 library(learnr)
 
+hmp = read.csv('~/Box Sync/knights_box/cp_wgs_data/hmp_analysis/taxoncounts_hmp_tonguestool_refcomplete.csv', header=1, row.names = 1, check.names = F, as.is = T)
+hmp = data.frame(t(hmp))
+hmp = hmp %>% setNames(sub('k__', '', names(.))) %>%
+  setNames(sub('.p__', ';', names(.))) %>%
+  setNames(sub('.c__', ';', names(.))) %>%
+  setNames(sub('.o__', ';', names(.))) %>%
+  setNames(sub('.f__', ';', names(.))) %>%
+  setNames(sub('.g__', ';', names(.))) %>%
+  setNames(sub('.s__', ';', names(.))) %>%
+  setNames(sub('.t__', ';', names(.)))
+
+hmp[is.na(hmp)] = 0
+hmp = tibble::rownames_to_column(hmp, var = 'SampleID')
+hmp.tall = hmp %>% gather(key='organism', value='abundance', 2:as.numeric(ncol(hmp)))
+hmp.split = hmp.tall %>% separate(organism, c('kingdom','phylum','class','order','family','genus','species','strain'), sep = ';')
+
